9286017508b14b64d776ca17c51d8d37a834ef75
Paula Lima <paula.lima@rosariosoftware.com>
2021-07-23 11:59:31 -0300

    adding asgi app

--- core/settings.py
+++ core/settings.py
@@ -115,3 +115,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = 'core.routing.application'
